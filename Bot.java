81429dd661bdae9634700dfdc7d7385b133055b3
meganf6 <meganf6@users.noreply.github.com>
2020-05-22 15:58:58 -0700

    Added methods getUsername and getPassword

--- Bot.java
+++ Bot.java
@@ -2,9 +2,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.Scanner.*;
 
 public class Bot{
     public static void main(String[] args) {
+        
         // Set up ChromeDriver
     	System.setProperty("webdriver.chrome.driver","chromedriver.exe");
         WebDriver driver = new ChromeDriver();
@@ -12,7 +14,26 @@ public class Bot{
 
         String url = "https://www.instagram.com/";
 
-        driver.findElement(By.xpath("//input[@name=\"name\"]")).sendKeys("");
+        //driver.findElement(By.xpath("//input[@name=\"name\"]")).sendKeys("");
+
+    }
+     
+    public String getUsername(){
+        Scanner scan = new Scanner(System.in);
+        String username;
+        System.out.println("Enter your username: ");
+        username = scan.next();
+        return username;
+        scan.close();
+    }
+
+    public String getPassword(){
+        Scanner scan = new Scanner(System.in);
+        String password;
+        System.out.println("Enter your password: ");
+        password = scan.next();
+        return password;
+        scan.close();
     }
 
 
