60f87e6b418a539ababf9fa3e64048814bac88ef
Ming Fong <mingfong07@hotmail.com>
2020-05-22 15:21:30 -0700

    ig

--- Bot.java
+++ Bot.java
@@ -3,26 +3,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Bot{
     public static void main(String[] args) {
-        // declaration and instantiation of objects/variables
+        // Set up ChromeDriver
     	System.setProperty("webdriver.chrome.driver","chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		//comment the above 2 lines and uncomment below 2 lines to use Chrome
-		//System.setProperty("webdriver.chrome.driver","G:\\chromedriver.exe");
-		//WebDriver driver = new ChromeDriver();
-    	
-        String url = "https://evilpegasus.github.io/";
-        String title = "";
+        WebDriver driver = new ChromeDriver();
+        
 
-        // launch Chrome and direct it to the Base URL
-        driver.get(url);
-
-        // get the actual value of the title
-        title = driver.getTitle();
-        System.out.println("The title of the page is" + title);
-       
-        //close Chrome
-        //driver.close();
-       
+        String url = "https://www.instagram.com/";
     }
 
 
